b1c9fad5bd6fda8a5588ef357eccf5a9ac4d2dab
stefany.forner <stefany.heringerforner@nomitri.com>
2022-09-27 00:13:31 +0200

    Highest Scoring Word

--- src/HighestScoringWord.java
+++ src/HighestScoringWord.java
@@ -11,4 +11,37 @@ import java.util.HashMap;
 
 public class HighestScoringWord {
 
+    public static String high(String s) {
+        HashMap<Character, Integer> lettersScore = new HashMap<>();
+        char[] alphabetLetters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        int highestScore = 0;
+        int secondHighestScore = 0;
+        String highestScoreWord = "";
+
+        int score = 1;
+        for (char alphabetLetter : alphabetLetters) {
+            lettersScore.put(alphabetLetter, score++);
+        }
+
+        String[] words = s.toLowerCase().split(" ");
+        char[] firstWord = words[0].toCharArray();
+
+        for (char c : firstWord) {
+            if (lettersScore.get(c) != null) highestScore += lettersScore.get(c);
+            highestScoreWord = words[0];
+        }
+
+        for (String word : words) {
+            char[] wordArray = word.toCharArray();
+            for (char letter : wordArray) {
+                if (lettersScore.get(letter) != null) secondHighestScore += lettersScore.get(letter);
+            }
+            if (secondHighestScore > highestScore) {
+                highestScore = secondHighestScore;
+                highestScoreWord = word;
+            }
+            secondHighestScore = 0;
+        }
+        return highestScoreWord;
+    }
 }
